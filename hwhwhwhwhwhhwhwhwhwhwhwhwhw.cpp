dc087cb7ac17bbaf5fd98e95d06b47902e0b440f
veronica <laurens.boychuk@gmail.com>
2022-07-04 13:40:27 +0200

    Add project files.

---
+++ hwhwhwhwhwhhwhwhwhwhwhwhwhw.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+//2
+
+//int main() 
+//{
+//    int digit, sum;
+//    cout << "Enter ur number:\n";
+//    cin >> digit;
+//    cout << "Ur number but opposite:";
+//
+//        while (digit > 0) {
+//            sum = digit % 10;
+//            digit /= 10;
+//
+//           cout << sum;
+//    }
+//}
+
+//3
+
+int main()
+{
+
+	int a;
+
+	cout << "Enter ur number:";
+	cin >> a;
+
+	for (unsigned int i = 1; i <= a >> 1; ++i) 
+		if (a % i); 
+		else 
+			cout << ' ' << i;
+}
